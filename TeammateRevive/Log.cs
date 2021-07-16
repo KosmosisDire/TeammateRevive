294ad6231fec5df965dc6da0efe9da05289954cf
NathaFred <kosmosisdire@gmail.com>
2021-07-16 13:34:55 -0400

    added changes from fork

--- TeammateRevive/Log.cs
+++ TeammateRevive/Log.cs
@@ -1,7 +1,4 @@
 ﻿using BepInEx.Logging;
-using System;
-using System.IO;
-using System.Text;
 
 namespace TeammateRevive
 {
@@ -9,12 +6,28 @@ namespace TeammateRevive
     {
         internal static ManualLogSource _logSource;
 
-        internal static void Init(ManualLogSource logSource) => _logSource = logSource;
+        internal static void Init(ManualLogSource logSource)
+        {
+            _logSource = logSource;
+        }
+
         internal static void LogDebug(object data) => _logSource.LogDebug(data);
-        internal static void LogError(object data) => _logSource.LogError(data);
+        internal static void LogError(object data)
+        {
+            _logSource.LogError(data);
+            DebugLogger.LogError(data.ToString());
+        }
         internal static void LogFatal(object data) => _logSource.LogFatal(data);
-        internal static void LogInfo(object data) => _logSource.LogInfo(data);
+        internal static void LogInfo(object data)
+        {
+            _logSource.LogInfo(data);
+            DebugLogger.LogInfo(data.ToString());
+        }
         internal static void LogMessage(object data) => _logSource.LogMessage(data);
-        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+        internal static void LogWarning(object data)
+        {
+            _logSource.LogWarning(data);
+            DebugLogger.LogWarning(data.ToString());
+        }
     }
 }
