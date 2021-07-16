5b4f6f8100568778c14bd6a53bcd3285848face2
NathaFred <kosmosisdire@gmail.com>
2021-07-16 13:41:36 -0400

    updated log prefixes

--- TeammateRevive/DebugLogger.cs
+++ TeammateRevive/DebugLogger.cs
@@ -6,35 +6,35 @@ public class DebugLogger
 {
     public static void diagInit()
     {
-        
         using (StreamWriter writer = new StreamWriter(System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\TeammateRevivalLog.txt"))
         {
             writer.WriteLine("Setup Teammate Revival" + DateTime.Now + '\n');
         }
-
     }
+
     public static void LogInfo(string message)
     {
         using (StreamWriter writer = new StreamWriter(System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\log.txt", true))
         {
-            writer.WriteLine(message.ToString() + DateTime.Now + '\n');
+            writer.WriteLine("[Info] " + message.ToString() + DateTime.Now + '\n');
         }
-
     }
+
     //Yellow Errors Prefixed With *WARNING* in Log File 
     public static void LogWarning(string message)
     {
         using (StreamWriter writer = new StreamWriter(System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\log.txt", true))
         {
-            writer.WriteLine("*WARNING*" + message.ToString() + DateTime.Now + '\n');
+            writer.WriteLine("[WARNING] " + message.ToString() + DateTime.Now + '\n');
         }
     }
+
     //Red Errors Prefixed With *ERROR* in Log File
     public static void LogError(string message)
     {
         using (StreamWriter writer = new StreamWriter(System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\log.txt", true))
         {
-            writer.WriteLine("*ERROR*" + message.ToString() + DateTime.Now + '\n');
+            writer.WriteLine("[ERROR] " + message.ToString() + DateTime.Now + '\n');
         }
     }
 }
--- TeammateRevive/TeammateRevive.cs
+++ TeammateRevive/TeammateRevive.cs
@@ -80,7 +80,7 @@ namespace TeammateRevive
             On.RoR2.SurvivorPodController.OnPassengerExit += hook_OnPassengerExit;
             On.RoR2.Run.OnUserAdded += hook_OnUserAdded;
             On.RoR2.Run.OnUserRemoved += Run_OnUserRemoved;
-            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += PlayerDied;
+            On.RoR2.GlobalEventManager.OnPlayerCharacterDeath += OnPlayerCharacterDeath;
             On.RoR2.Run.BeginGameOver += hook_BeginGameOver;
             On.RoR2.Run.AdvanceStage += hook_AdvanceStage;
             
@@ -243,7 +243,7 @@ namespace TeammateRevive
         }
 
 
-        public void PlayerDied (On.RoR2.GlobalEventManager.orig_OnPlayerCharacterDeath orig, global::RoR2.GlobalEventManager self, global::RoR2.DamageReport damageReport, global::RoR2.NetworkUser victimNetworkUser)
+        public void OnPlayerCharacterDeath (On.RoR2.GlobalEventManager.orig_OnPlayerCharacterDeath orig, global::RoR2.GlobalEventManager self, global::RoR2.DamageReport damageReport, global::RoR2.NetworkUser victimNetworkUser)
         {
             orig(self, damageReport, victimNetworkUser);
 
