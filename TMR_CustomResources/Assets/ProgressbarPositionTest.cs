02af18a9cfe876e20d89e9603af65892cdd9249e
KosmosisDire <kosmosisdire@gmail.com>
2022-05-04 18:52:45 -0400

    TMR_Resources progress bar fixes / visuals

---
+++ TMR_CustomResources/Assets/ProgressbarPositionTest.cs
@@ -0,0 +1,85 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProgressbarPositionTest : MonoBehaviour
+{
+    public RectTransform mainUIPanel;
+
+    RectTransform healthbarTransform;
+    RectTransform barRootTransform;
+    RectTransform progressBarTransform;
+
+    public (Vector3 bottomLeftOffset, Vector2 size) GetBarPositionAndSize()
+    {
+        //find and set parent to the center cluster
+        if(progressBarTransform.parent.name != "BottomCenterCluster")
+        {
+            var cluster = mainUIPanel.transform.Find("SpringCanvas/BottomCenterCluster");
+            if(cluster != null)
+            {
+                progressBarTransform.SetParent(cluster);
+            }
+            else
+            {
+                //fallback to the main panel
+                progressBarTransform.SetParent(mainUIPanel.transform);
+            }
+        }
+
+        Vector2 parentSize = progressBarTransform.parent.GetComponent<RectTransform>().rect.size;
+
+        //fallback values in case healthbar is not found
+        Vector2 size = new Vector2(Screen.width/3.5f, Screen.height/27f);
+        Vector3 bottomLeftOffset = new Vector3(parentSize.x/2 - size.x/2, size.y * 6, 0);
+
+        var healthBarRoot = mainUIPanel.transform.Find("SpringCanvas/BottomLeftCluster/BarRoots/HealthbarRoot");
+        var barRoots = mainUIPanel.transform.Find("SpringCanvas/BottomLeftCluster/BarRoots");
+
+        if (healthBarRoot == null || barRoots == null)
+        {
+            return (bottomLeftOffset, size);
+        }
+
+        if(healthbarTransform == null || barRootTransform == null)
+        {
+            healthbarTransform = healthBarRoot.GetComponent<RectTransform>();
+            barRootTransform = barRoots.GetComponent<RectTransform>();
+        }
+
+        size = new Vector2(parentSize.x * 0.8f, healthbarTransform.rect.height);
+
+        //use law of sines to get the depth of the healthbar after 6 degrees of rotation
+        float depthOffset = barRootTransform.rect.width
+                            / Mathf.Sin(90 * Mathf.Deg2Rad) 
+                            * Mathf.Sin(-barRoots.parent.rotation.eulerAngles.y * Mathf.Deg2Rad);
+
+
+        Debug.Log(healthbarTransform.GetBottomLeftOffset());
+        bottomLeftOffset = new Vector3(parentSize.x/2 - size.x/2, healthbarTransform.GetBottomLeftOffset().y, depthOffset);
+
+        return (bottomLeftOffset, size);
+    }
+
+    public void UpdatePositionAndSize()
+    {
+        var (bottomLeftOffset, size) = GetBarPositionAndSize();
+
+        progressBarTransform.SetSizeInPixels(size.x, size.y);
+        progressBarTransform.SetBottomLeftOffset(bottomLeftOffset.x, bottomLeftOffset.y);
+        progressBarTransform.localScale = Vector3.one;
+        progressBarTransform.localPosition = progressBarTransform.localPosition.SetZ(bottomLeftOffset.z);
+    }
+
+    void Update()
+    {
+        UpdatePositionAndSize();
+    }
+
+    void Start()
+    {
+        progressBarTransform = GetComponent<RectTransform>();
+    }
+}
+
---
+++ TMR_CustomResources/Assets/VectorExtentions.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class VectorExtentions
+{
+    public static Vector2 MirrorY(this Vector2 v, RectTransform space, bool negateY = true)
+    {
+        return new Vector2(v.x, (negateY ? -1 : 1) * (space.rect.height - v.y));
+    }
+
+    public static Vector2 MirrorY(this Vector3 v, RectTransform space, bool negateY = true)
+    {
+        return new Vector2(v.x, (negateY ? -1 : 1) * (space.rect.height - v.y));
+    }
+
+    public static Vector2 MirrorX(this Vector2 v, RectTransform space, bool negateX = true)
+    {
+        return new Vector2((negateX ? -1 : 1) * (space.rect.width - v.x), v.y);
+    }
+
+    public static Vector2 MirrorX(this Vector3 v, RectTransform space, bool negateX = true)
+    {
+        return new Vector2((negateX ? -1 : 1) * (space.rect.width - v.x), v.y);
+    }
+
+    public static Vector2 MultiplyComponent (this Vector2 v, Vector2 other)
+    {
+        return new Vector2(v.x * other.x, v.y * other.y);
+    }
+
+    public static Vector3 MultiplyComponent (this Vector3 v, Vector3 other)
+    {
+        return new Vector3(v.x * other.x, v.y * other.y, v.z * other.z);
+    }
+
+    public static Vector2 DivideComponent (this Vector2 v, Vector2 other)
+    {
+        return new Vector2(v.x / other.x, v.y / other.y);
+    }
+
+    public static Vector3 DivideComponent (this Vector3 v, Vector3 other)
+    {
+        return new Vector3(v.x / other.x, v.y / other.y, v.z / other.z);
+    }
+
+    public static Vector3 SetX(this Vector3 v, float x)
+    {
+        return new Vector3(x, v.y, v.z);
+    }
+
+    public static Vector3 SetY(this Vector3 v, float y)
+    {
+        return new Vector3(v.x, y, v.z);
+    }
+
+    public static Vector3 SetZ(this Vector3 v, float z)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
+
+    public static Vector2 XY(this Vector3 v)
+    {
+        return new Vector2(v.x, v.y);
+    }
+
+    public static Vector2 XZ(this Vector3 v)
+    {
+        return new Vector2(v.x, v.z);
+    }
+
+    public static Vector2 YZ(this Vector3 v)
+    {
+        return new Vector2(v.y, v.z);
+    }
+
+    
+}
